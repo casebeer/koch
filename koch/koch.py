5c1ec7238ad217e165b7ab72a302a6e5747c5ca1
Christopher H. Casebeer <christopher@chc.name>
2026-02-15 20:01:14 -0500

    [whitespace] Convert tabs to spaces per new PEP-8

--- koch/koch.py
+++ koch/koch.py
@@ -27,181 +27,181 @@ DEFAULT_WPM = 20
 DEFAULT_FARNSWORTH_CUTOFF = 20
 
 def koch_alphabet(characters=2):
-	# todo: parse alphabet from string, handling prosigns properly
-	# todo: accept and parse custom alphabet string
-	alphabet = [char for char in "KMRSUAPTLOWI.NJEF0Y,VG5/Q9ZH38B?427C1D6X"] +\
-		["BT", "SK", "AR"]
-	return alphabet[:characters]
+    # todo: parse alphabet from string, handling prosigns properly
+    # todo: accept and parse custom alphabet string
+    alphabet = [char for char in "KMRSUAPTLOWI.NJEF0Y,VG5/Q9ZH38B?427C1D6X"] +\
+        ["BT", "SK", "AR"]
+    return alphabet[:characters]
 
 def koch(length=20, alphabet=koch_alphabet(2)):
-	def insert_spaces(letters):
-		# first char can't be a space
-		# todo: better word length distribution
-		yield next(letters)
-		for letter in letters:
-			if random.random() < 1. / 4:
-				yield u" "
-			yield letter
+    def insert_spaces(letters):
+        # first char can't be a space
+        # todo: better word length distribution
+        yield next(letters)
+        for letter in letters:
+            if random.random() < 1. / 4:
+                yield u" "
+            yield letter
 
-	letters = (random.choice(alphabet) for i in range(length))
-	return u"".join(insert_spaces(letters))
+    letters = (random.choice(alphabet) for i in range(length))
+    return u"".join(insert_spaces(letters))
 
 def read_stdin():
-	for line in sys.stdin:
-		for char in line.strip("\n"):
-			yield char.upper()
+    for line in sys.stdin:
+        for char in line.strip("\n"):
+            yield char.upper()
 def main():
-	import argparse
+    import argparse
 
-	parser = argparse.ArgumentParser(
-		formatter_class=argparse.ArgumentDefaultsHelpFormatter
-	)
-	parser.add_argument("-l", "--length",
-		type=int,
-		default=DEFAULT_LENGTH,
-		help="Length of practise message in characters."
-	)
-	parser.add_argument("-c", "--characters",
-		type=int,
-		default=DEFAULT_TRAINING_CHARACTERS,
-		help="Number of distinct characters to practise."
-	)
-	parser.add_argument("-i", "--intro",
-		action="store_true",
-		default=False,
-		help="Play just the Nth Koch character to introduce it. Set `N` with the -c flag."
-	)
-	parser.add_argument("-a", "--custom-alphabet",
-		type=str,
-		default=None,
-		help="Custom alphabet to use in place of default Koch ordering."
-	)
-	# todo: make saving in lieu of playing?
-	parser.add_argument("-f", "--file",
-		type=str,
-		default=None,
-		help="Save audio to a WAV file."
-	)
-	parser.add_argument("-H", "--hertz",
-		type=float,
-		default=DEFAULT_HERTZ,
-		help="Frequency in Hertz to use for practise tones."
-	)
-	parser.add_argument("-B", "--bandwidth",
-		type=float,
-		default=DEFAULT_BANDWIDTH,
-		help="Audio bandwidth in Hertz, centered on the tone frequency."
-	)
-	parser.add_argument("-w", "--wpm",
-		type=float,
-		default=DEFAULT_WPM,
-		help="Morse words per minute."
-	)
-	parser.add_argument("--cwpm",
-		type=float,
-		default=None,
-		help=f"""Morse *character* words per minute.
-		If unset, defaults to max({DEFAULT_FARNSWORTH_CUTOFF}, WPM).
-		"""
-	)
-	parser.add_argument("--forever",
-		action="store_true",
-		default=False,
-		help="""Loop the message forever (cannot be combined with --file)."""
-	)
-	parser.add_argument("-d", "--debug",
-		action="store_true",
-		default=False
-	)
-	parser.add_argument("-s", "--stdin",
-		action="store_true",
-		default=False,
-		help="""Read message from stdin."""
-	)
-	parser.add_argument("message", nargs="*", default=None)
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("-l", "--length",
+        type=int,
+        default=DEFAULT_LENGTH,
+        help="Length of practise message in characters."
+    )
+    parser.add_argument("-c", "--characters",
+        type=int,
+        default=DEFAULT_TRAINING_CHARACTERS,
+        help="Number of distinct characters to practise."
+    )
+    parser.add_argument("-i", "--intro",
+        action="store_true",
+        default=False,
+        help="Play just the Nth Koch character to introduce it. Set `N` with the -c flag."
+    )
+    parser.add_argument("-a", "--custom-alphabet",
+        type=str,
+        default=None,
+        help="Custom alphabet to use in place of default Koch ordering."
+    )
+    # todo: make saving in lieu of playing?
+    parser.add_argument("-f", "--file",
+        type=str,
+        default=None,
+        help="Save audio to a WAV file."
+    )
+    parser.add_argument("-H", "--hertz",
+        type=float,
+        default=DEFAULT_HERTZ,
+        help="Frequency in Hertz to use for practise tones."
+    )
+    parser.add_argument("-B", "--bandwidth",
+        type=float,
+        default=DEFAULT_BANDWIDTH,
+        help="Audio bandwidth in Hertz, centered on the tone frequency."
+    )
+    parser.add_argument("-w", "--wpm",
+        type=float,
+        default=DEFAULT_WPM,
+        help="Morse words per minute."
+    )
+    parser.add_argument("--cwpm",
+        type=float,
+        default=None,
+        help=f"""Morse *character* words per minute.
+        If unset, defaults to max({DEFAULT_FARNSWORTH_CUTOFF}, WPM).
+        """
+    )
+    parser.add_argument("--forever",
+        action="store_true",
+        default=False,
+        help="""Loop the message forever (cannot be combined with --file)."""
+    )
+    parser.add_argument("-d", "--debug",
+        action="store_true",
+        default=False
+    )
+    parser.add_argument("-s", "--stdin",
+        action="store_true",
+        default=False,
+        help="""Read message from stdin."""
+    )
+    parser.add_argument("message", nargs="*", default=None)
+    args = parser.parse_args()
 
-	if args.debug:
-		logging.basicConfig(level=logging.DEBUG)
-	else:
-		logging.basicConfig(level=logging.WARN)
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.WARN)
 
-	if args.forever and args.file:
-		print("Cannot write an infinitely looping audio stream to a file.")
-		sys.exit(1)
+    if args.forever and args.file:
+        print("Cannot write an infinitely looping audio stream to a file.")
+        sys.exit(1)
 
-	if args.custom_alphabet:
-		# todo: parse with koch_alphabet function (TBD)
-		alphabet = args.custom_alphabet[:args.characters]
-	else:
-		alphabet = koch_alphabet(args.characters)
+    if args.custom_alphabet:
+        # todo: parse with koch_alphabet function (TBD)
+        alphabet = args.custom_alphabet[:args.characters]
+    else:
+        alphabet = koch_alphabet(args.characters)
 
-	if args.message:
-		# play manually specified message
-		message = u" ".join(args.message).upper()
-	elif args.stdin:
-		# read message from stdin
-		message = read_stdin()
-	elif args.intro:
-		# play the Nth char length times to teach the char
-		message = alphabet[-1] * args.length
-	else:
-		# play random message of chosen chars and length
-		message = koch(args.length, alphabet)
+    if args.message:
+        # play manually specified message
+        message = u" ".join(args.message).upper()
+    elif args.stdin:
+        # read message from stdin
+        message = read_stdin()
+    elif args.intro:
+        # play the Nth char length times to teach the char
+        message = alphabet[-1] * args.length
+    else:
+        # play random message of chosen chars and length
+        message = koch(args.length, alphabet)
 
-	if args.cwpm:
-		cwpm = args.cwpm
-	else:
-		# Farnsworth timings below 20 WPM  (n.b. ARRL cutoff is 18 WPM)
-		cwpm = max(DEFAULT_FARNSWORTH_CUTOFF, args.wpm)
+    if args.cwpm:
+        cwpm = args.cwpm
+    else:
+        # Farnsworth timings below 20 WPM  (n.b. ARRL cutoff is 18 WPM)
+        cwpm = max(DEFAULT_FARNSWORTH_CUTOFF, args.wpm)
 
-	if args.intro or args.message:
-		print(message)
-	elif args.stdin:
-		print("Reading from stdin...")
-	else:
-		if int(args.wpm) == cwpm:
-			wpm_message = f"({int(args.wpm)} WPM)"
-		else:
-			wpm_message = f"({int(args.wpm)} WPM/{cwpm} CWPM)"
-		print(f"Testing characters {wpm_message}:\n{u'·'.join(alphabet)}")
+    if args.intro or args.message:
+        print(message)
+    elif args.stdin:
+        print("Reading from stdin...")
+    else:
+        if int(args.wpm) == cwpm:
+            wpm_message = f"({int(args.wpm)} WPM)"
+        else:
+            wpm_message = f"({int(args.wpm)} WPM/{cwpm} CWPM)"
+        print(f"Testing characters {wpm_message}:\n{u'·'.join(alphabet)}")
 
 
-	if args.forever:
-		print("Hit ctrl-c to exit")
+    if args.forever:
+        print("Hit ctrl-c to exit")
 
-	with audiogen.sampler.frame_rate(48000):
-		with morse.timings(morse.farnsworth(args.wpm, cwpm)):
-			with morse.tone(args.hertz):
-				with morse.bandwidth(args.bandwidth):
-					# todo: determine nesting behavior of context managers
+    with audiogen.sampler.frame_rate(48000):
+        with morse.timings(morse.farnsworth(args.wpm, cwpm)):
+            with morse.tone(args.hertz):
+                with morse.bandwidth(args.bandwidth):
+                    # todo: determine nesting behavior of context managers
 
-					if args.debug:
-						# generate audio samples just for visualization, since visualizing consumes
-						# them and they will not be available for audio output
-						logger.debug(morse.visualize_samples(morse.code(message)))
+                    if args.debug:
+                        # generate audio samples just for visualization, since visualizing consumes
+                        # them and they will not be available for audio output
+                        logger.debug(morse.visualize_samples(morse.code(message)))
 
-					audio = morse.code(message)
-					if args.file:
-						with open(args.file, "wb") as f:
-							audiogen.write_wav(f, audio)
-					else:
-						try:
-							if args.forever:
-								audio = itertools.cycle(itertools.chain(audio, morse.code(" ")))
-							stream = audiogen.sampler.play(
-								itertools.chain(audio, morse.code(" "), audiogen.beep()),
-								blocking=True
-							)
-						except KeyboardInterrupt:
-							# So further messages don't start with "^C"
-							print(u"")
+                    audio = morse.code(message)
+                    if args.file:
+                        with open(args.file, "wb") as f:
+                            audiogen.write_wav(f, audio)
+                    else:
+                        try:
+                            if args.forever:
+                                audio = itertools.cycle(itertools.chain(audio, morse.code(" ")))
+                            stream = audiogen.sampler.play(
+                                itertools.chain(audio, morse.code(" "), audiogen.beep()),
+                                blocking=True
+                            )
+                        except KeyboardInterrupt:
+                            # So further messages don't start with "^C"
+                            print(u"")
 
-	if not args.intro and not args.message and not args.file and not args.stdin:
-		input(u"\nHit <enter> to see correct transcription...")
-		print(u"\n{}".format(message.lower()))
+    if not args.intro and not args.message and not args.file and not args.stdin:
+        input(u"\nHit <enter> to see correct transcription...")
+        print(u"\n{}".format(message.lower()))
 
-	return 0
+    return 0
 
 if __name__ == "__main__":
-	sys.exit(main())
+    sys.exit(main())
--- setup.py
+++ setup.py
@@ -3,38 +3,38 @@
 from setuptools import setup, find_packages
 
 required_modules = [
-	'audiogen',
-	'PyAudio',
-	]
+    'audiogen',
+    'PyAudio',
+    ]
 
 with open("README.rst", "r", encoding="utf-8") as f:
-	readme = f.read()
+    readme = f.read()
 
 setup(
-	name="koch",
-	version="0.0.3",
-	description="Koch method Morse code training program",
-	author="Christopher H. Casebeer",
-	author_email="",
-	url="https://github.com/casebeer/koch",
+    name="koch",
+    version="0.0.3",
+    description="Koch method Morse code training program",
+    author="Christopher H. Casebeer",
+    author_email="",
+    url="https://github.com/casebeer/koch",
 
-	packages=find_packages(exclude='tests'),
-	install_requires=required_modules,
+    packages=find_packages(exclude='tests'),
+    install_requires=required_modules,
 
-	tests_require=["nose"],
-	test_suite="nose.collector",
+    tests_require=["nose"],
+    test_suite="nose.collector",
 
-	entry_points={
-		"console_scripts": [
-			"koch = koch.koch:main"
-		]
-	},
+    entry_points={
+        "console_scripts": [
+            "koch = koch.koch:main"
+        ]
+    },
 
-	long_description=readme,
-	classifiers=[
-		"Environment :: Console",
-		"Topic :: Multimedia :: Sound/Audio",
-		"Topic :: Communications :: Ham Radio",
-		"Programming Language :: Python :: 2.7",
-	]
+    long_description=readme,
+    classifiers=[
+        "Environment :: Console",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Communications :: Ham Radio",
+        "Programming Language :: Python :: 2.7",
+    ]
 )
