ee15ac7b3a852be8e99503f000170a46cdc78c65
Christopher H. Casebeer <christopher@chc.name>
2026-02-15 20:36:00 -0500

    Linter fixes

--- koch/koch.py
+++ koch/koch.py
@@ -1,16 +1,16 @@
-
-# coding=utf8
-
+import argparse
+import itertools
 import logging
-logger = logging.getLogger(__name__)
+import random
+import sys
+from collections.abc import Iterable, Iterator
 
 import audiogen
-import koch.morse as morse
 
-import itertools
-import random
+from koch import morse
 
-import sys
+logger = logging.getLogger(__name__)
+
 
 # http://www.qsl.net/n1irz/finley.morse.html
 # http://www.codepractice.com/learning.html
@@ -26,97 +26,118 @@ DEFAULT_WPM = 20
 # Farnsworth timings below 20 WPM  (n.b. ARRL cutoff is 18 WPM)
 DEFAULT_FARNSWORTH_CUTOFF = 20
 
-def koch_alphabet(characters=2):
-    # todo: parse alphabet from string, handling prosigns properly
-    # todo: accept and parse custom alphabet string
-    alphabet = [char for char in "KMRSUAPTLOWI.NJEF0Y,VG5/Q9ZH38B?427C1D6X"] +\
-        ["BT", "SK", "AR"]
-    return alphabet[:characters]
+KOCH_ALPHABET = [*list("KMRSUAPTLOWI.NJEF0Y,VG5/Q9ZH38B?427C1D6X"), *["BT", "SK", "AR"]]
 
-def koch(length=20, alphabet=koch_alphabet(2)):
-    def insert_spaces(letters):
+
+def koch(length: int = 20, alphabet: list[str] = KOCH_ALPHABET[:2]) -> str:
+    def insert_spaces(letters: Iterator[str]) -> Iterator[str]:
         # first char can't be a space
-        # todo: better word length distribution
+        # TODO: better word length distribution
         yield next(letters)
         for letter in letters:
-            if random.random() < 1. / 4:
-                yield u" "
+            if random.random() < 1.0 / 4:
+                yield " "
             yield letter
 
     letters = (random.choice(alphabet) for i in range(length))
-    return u"".join(insert_spaces(letters))
+    return "".join(insert_spaces(letters))
 
-def read_stdin():
+
+def read_stdin() -> Iterator[str]:
     for line in sys.stdin:
         for char in line.strip("\n"):
             yield char.upper()
-def main():
-    import argparse
+
+
+def main() -> int:
 
     parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument("-l", "--length",
+    parser.add_argument(
+        "-l",
+        "--length",
         type=int,
         default=DEFAULT_LENGTH,
-        help="Length of practise message in characters."
+        help="Length of practise message in characters.",
     )
-    parser.add_argument("-c", "--characters",
+    parser.add_argument(
+        "-c",
+        "--characters",
         type=int,
         default=DEFAULT_TRAINING_CHARACTERS,
-        help="Number of distinct characters to practise."
+        help="Number of distinct characters to practise.",
     )
-    parser.add_argument("-i", "--intro",
+    parser.add_argument(
+        "-i",
+        "--intro",
         action="store_true",
         default=False,
-        help="Play just the Nth Koch character to introduce it. Set `N` with the -c flag."
+        help="Play just the Nth Koch character to introduce it. "
+        "Set `N` with the -c flag.",
     )
-    parser.add_argument("-a", "--custom-alphabet",
+    parser.add_argument(
+        "-a",
+        "--custom-alphabet",
         type=str,
         default=None,
-        help="Custom alphabet to use in place of default Koch ordering."
+        help="Custom alphabet to use in place of default Koch ordering.",
     )
-    # todo: make saving in lieu of playing?
-    parser.add_argument("-f", "--file",
+    # TODO: make saving in lieu of playing?
+    parser.add_argument(
+        "-f",
+        "--file",
         type=str,
         default=None,
-        help="Save audio to a WAV file."
+        help="Save audio to a WAV file.",
     )
-    parser.add_argument("-H", "--hertz",
+    parser.add_argument(
+        "-H",
+        "--hertz",
         type=float,
         default=DEFAULT_HERTZ,
-        help="Frequency in Hertz to use for practise tones."
+        help="Frequency in Hertz to use for practise tones.",
     )
-    parser.add_argument("-B", "--bandwidth",
+    parser.add_argument(
+        "-B",
+        "--bandwidth",
         type=float,
         default=DEFAULT_BANDWIDTH,
-        help="Audio bandwidth in Hertz, centered on the tone frequency."
+        help="Audio bandwidth in Hertz, centered on the tone frequency.",
     )
-    parser.add_argument("-w", "--wpm",
+    parser.add_argument(
+        "-w",
+        "--wpm",
         type=float,
         default=DEFAULT_WPM,
-        help="Morse words per minute."
+        help="Morse words per minute.",
     )
-    parser.add_argument("--cwpm",
+    parser.add_argument(
+        "--cwpm",
         type=float,
         default=None,
         help=f"""Morse *character* words per minute.
         If unset, defaults to max({DEFAULT_FARNSWORTH_CUTOFF}, WPM).
-        """
+        """,
     )
-    parser.add_argument("--forever",
+    parser.add_argument(
+        "--forever",
         action="store_true",
         default=False,
-        help="""Loop the message forever (cannot be combined with --file)."""
+        help="""Loop the message forever (cannot be combined with --file).""",
     )
-    parser.add_argument("-d", "--debug",
-        action="store_true",
-        default=False
-    )
-    parser.add_argument("-s", "--stdin",
+    parser.add_argument(
+        "-d",
+        "--debug",
         action="store_true",
         default=False,
-        help="""Read message from stdin."""
+    )
+    parser.add_argument(
+        "-s",
+        "--stdin",
+        action="store_true",
+        default=False,
+        help="""Read message from stdin.""",
     )
     parser.add_argument("message", nargs="*", default=None)
     args = parser.parse_args()
@@ -124,21 +145,20 @@ def main():
     if args.debug:
         logging.basicConfig(level=logging.DEBUG)
     else:
-        logging.basicConfig(level=logging.WARN)
+        logging.basicConfig(level=logging.WARNING)
 
     if args.forever and args.file:
         print("Cannot write an infinitely looping audio stream to a file.")
         sys.exit(1)
 
     if args.custom_alphabet:
-        # todo: parse with koch_alphabet function (TBD)
-        alphabet = args.custom_alphabet[:args.characters]
+        alphabet = args.custom_alphabet[: args.characters]
     else:
-        alphabet = koch_alphabet(args.characters)
+        alphabet = KOCH_ALPHABET[: args.characters]
 
     if args.message:
         # play manually specified message
-        message = u" ".join(args.message).upper()
+        message: Iterable[str] = " ".join(args.message).upper()
     elif args.stdin:
         # read message from stdin
         message = read_stdin()
@@ -164,44 +184,42 @@ def main():
             wpm_message = f"({int(args.wpm)} WPM)"
         else:
             wpm_message = f"({int(args.wpm)} WPM/{cwpm} CWPM)"
-        print(f"Testing characters {wpm_message}:\n{u'·'.join(alphabet)}")
-
+        print(f"Testing characters {wpm_message}:\n{'·'.join(alphabet)}")
 
     if args.forever:
         print("Hit ctrl-c to exit")
 
-    with audiogen.sampler.frame_rate(48000):
-        with morse.timings(morse.farnsworth(args.wpm, cwpm)):
-            with morse.tone(args.hertz):
-                with morse.bandwidth(args.bandwidth):
-                    # todo: determine nesting behavior of context managers
+    with audiogen.sampler.frame_rate(48000), morse.timings(
+        morse.farnsworth(args.wpm, cwpm)
+    ), morse.tone(args.hertz), morse.bandwidth(args.bandwidth):
 
-                    if args.debug:
-                        # generate audio samples just for visualization, since visualizing consumes
-                        # them and they will not be available for audio output
-                        logger.debug(morse.visualize_samples(morse.code(message)))
+        if args.debug:
+            # generate audio samples just for visualization, since visualizing consumes
+            # them and they will not be available for audio output
+            logger.debug(morse.visualize_samples(morse.code(message)))
 
-                    audio = morse.code(message)
-                    if args.file:
-                        with open(args.file, "wb") as f:
-                            audiogen.write_wav(f, audio)
-                    else:
-                        try:
-                            if args.forever:
-                                audio = itertools.cycle(itertools.chain(audio, morse.code(" ")))
-                            stream = audiogen.sampler.play(
-                                itertools.chain(audio, morse.code(" "), audiogen.beep()),
-                                blocking=True
-                            )
-                        except KeyboardInterrupt:
-                            # So further messages don't start with "^C"
-                            print(u"")
+        audio = morse.code(message)
+        if args.file:
+            with open(args.file, "wb") as f:
+                audiogen.write_wav(f, audio)
+        else:
+            try:
+                if args.forever:
+                    audio = itertools.cycle(itertools.chain(audio, morse.code(" ")))
+                audiogen.sampler.play(
+                    itertools.chain(audio, morse.code(" "), audiogen.beep()),
+                    blocking=True,
+                )
+            except KeyboardInterrupt:
+                # So further messages don't start with "^C"
+                print()
 
     if not args.intro and not args.message and not args.file and not args.stdin:
-        input(u"\nHit <enter> to see correct transcription...")
-        print(u"\n{}".format(message.lower()))
+        input("\nHit <enter> to see correct transcription...")
+        print(f"\n{str.lower(str(message))}")
 
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
--- koch/morse.py
+++ koch/morse.py
@@ -1,21 +1,20 @@
-
+import contextlib
+import itertools
 import logging
-logger = logging.getLogger(__name__)
+from collections.abc import Iterable
+from functools import reduce
 
 import audiogen
 import audiogen.util
 
-import itertools
-import contextlib
-
-from functools import reduce
+logger = logging.getLogger(__name__)
 
 HERTZ = 770
 BANDWIDTH = 200
 DEFAULT_WPM = 20
 
 #
-# todo: support parsing of prosigns
+# TODO: support parsing of prosigns
 #
 
 # ARRL Morse Transmission Timing Standard
@@ -27,29 +26,29 @@ DEFAULT_WPM = 20
 # or
 # s/DIT = 1.2 / WPM
 
-def wpm(wpm=20, farnsworth_limit=18):
-    '''
-    Generate Morse timings for specified WPM
+
+def wpm(wpm: int = 20, farnsworth_limit: int = 18):
+    """Generate Morse timings for specified WPM.
 
     For WPM values below farnsworth_limit, Farnsworth timing
     will be used per the ARRL standard. The default farnsworth_limit
     is 18 WPM. To disable Farnsworth timing entirely, set
     farnsworth_limit=None.
-    '''
+    """
     return farnsworth(
         wpm,
-        max(farnsworth_limit, wpm) if farnsworth_limit else wpm
+        max(farnsworth_limit, wpm) if farnsworth_limit else wpm,
     )
 
-def farnsworth(wpm=20, cwpm=None):
-    '''
-    Generate Morse timings based on ARRL Farnsworth timing
+
+def farnsworth(wpm: int = 20, cwpm=None):
+    """Generate Morse timings based on ARRL Farnsworth timing.
 
     This permits optionally specifying a different overall words
     per minute. The character WPM determines the timings of the
     dits, dahs, and intra-letter spaces; the overall WPM determines
     the timings of the inter-letter and inter-word spaces.
-    '''
+    """
     if not cwpm:
         cwpm = wpm
 
@@ -64,10 +63,10 @@ def farnsworth(wpm=20, cwpm=None):
         "dit": dit,
         "dah": dit * 3,
         "inter_symbol": dit,
-
-        "inter_letter": (t_a * 3) / 19.,
-        "inter_word": (t_a * 7) / 19.
+        "inter_letter": (t_a * 3) / 19.0,
+        "inter_word": (t_a * 7) / 19.0,
     }
+
 
 def set_times(timings):
     global DIT, DAH, INTER_SYMBOL, INTER_LETTER, INTER_WORD
@@ -77,43 +76,50 @@ def set_times(timings):
     INTER_LETTER = timings["inter_letter"]
     INTER_WORD = timings["inter_word"]
 
+
 def get_times():
     return {
         "dit": DIT,
         "dah": DAH,
         "inter_symbol": INTER_SYMBOL,
         "inter_letter": INTER_LETTER,
-        "inter_word": INTER_WORD
+        "inter_word": INTER_WORD,
     }
+
 
 # set default timings
 set_times(wpm(DEFAULT_WPM))
 
-class timings(object):
-    '''
-    Context manager to set Morse symbol timings.
-    '''
+
+class timings:
+    """Context manager to set Morse symbol timings."""
+
     def __init__(self, timings):
         self._new_timings = timings
-    def __enter__(self, ):
+
+    def __enter__(self):
         self._saved_timings = get_times()
         set_times(self._new_timings)
+
     def __exit__(self, *args, **kwargs):
         set_times(self._saved_timings)
 
-class tone(object):
-    '''
-    Context manager to set frequency for Morse tones.
-    '''
+
+class tone:
+    """Context manager to set frequency for Morse tones."""
+
     def __init__(self, frequency):
         self._new_frequency = frequency
-    def __enter__(self, ):
+
+    def __enter__(self):
         global HERTZ
         self._saved_frequency = HERTZ
         HERTZ = self._new_frequency
+
     def __exit__(self, *args, **kwargs):
         global HERTZ
         HERTZ = self._saved_frequency
+
 
 @contextlib.contextmanager
 def bandwidth(bandwidth):
@@ -123,30 +129,31 @@ def bandwidth(bandwidth):
     yield
     BANDWIDTH = old
 
+
 @audiogen.sampler.cache_finite_samples
 def tone_for(seconds):
-    tone = audiogen.crop(audiogen.util.volume(audiogen.tone(HERTZ), -3), seconds)
-    return tone
+    return audiogen.crop(audiogen.util.volume(audiogen.tone(HERTZ), -3), seconds)
+
 
 def dit():
-    for sample in tone_for(DIT):
-        yield sample
+    yield from tone_for(DIT)
+
 
 def dah():
-    for sample in tone_for(DAH):
-        yield sample
+    yield from tone_for(DAH)
+
 
 def space():
-    for sample in audiogen.silence(INTER_WORD):
-        yield sample
+    yield from audiogen.silence(INTER_WORD)
+
 
 def inter_symbol():
-    for sample in audiogen.silence(INTER_SYMBOL):
-        yield sample
+    yield from audiogen.silence(INTER_SYMBOL)
+
 
 def inter_letter():
-    for sample in audiogen.silence(INTER_LETTER):
-        yield sample
+    yield from audiogen.silence(INTER_LETTER)
+
 
 LETTERS = {
     "A": (dit, dah),
@@ -192,20 +199,23 @@ LETTERS = {
 }
 
 ### Add prosigns to LETTERS dict
-# todo: do we need to do this, or can the parser generate custom prosigns on the fly?
-PROSIGNS = [ "BT", "SK", "AR", "BK", "KN", "CL" ]
+# TODO: do we need to do this, or can the parser generate custom prosigns on the fly?
+PROSIGNS = ["BT", "SK", "AR", "BK", "KN", "CL"]
 for prosign in PROSIGNS:
     LETTERS[prosign] = reduce(lambda a, b: a + b, [LETTERS[char] for char in prosign])
 
-'''
-Implement str.join() for generators and lists
 
-Interleave output of `joiner_func()` between items of `joined`, without appending a joiner after the
-final item of `joined`.  Equivalent to `joiner.join(joined)`
-
-`joiner_func` must be a function which emits joiner.
-'''
 def gen_join(joiner_func, joined):
+    """
+    Implement str.join() for generators and lists
+
+    Interleave output of `joiner_func()` between items of `joined`,
+    without appending a joiner after the final item of `joined`.
+
+    Equivalent to `joiner.join(joined)`
+
+    `joiner_func` must be a function which emits joiner.
+    """
     items = iter(joined)
 
     # pull off the first item, if any
@@ -224,33 +234,45 @@ def gen_join(joiner_func, joined):
     # this is now the final item in the list, so emit it without a trailing space
     yield previous_item
 
-'''
-Produce printable visualization of sample data, with | for any sound and _ for silence
-Decimate by 1:512 to shorten samples to reasonable lengths for printing in console.
-n.b. We should do a moving average of all samples rather than decimating, but decimating is faster.
-     There may be glitches in the output as a result (e.g. showing silence due to a near-zero-
-     corssing in the middle of non-silence).
-'''
+
 def visualize_samples(samples):
-    return "".join(['_' if -.01 < sample < .001 else '|' for index, sample in enumerate(samples) if index % 512 == 0])
+    """
+    Produce printable visualization of sample data, using | and _ for sound and silence.
+
+    Decimate by 1:512 to shorten samples to reasonable lengths for printing in console.
+    n.b. We should do a moving average of all samples rather than decimating, but
+         decimating is faster. There may be glitches in the output as a result
+         (e.g. showing silence due to a near-zero-crossing in the middle
+         of non-silence).
+    """
+    return "".join(
+        [
+            "_" if -0.01 < sample < 0.001 else "|"
+            for index, sample in enumerate(samples)
+            if index % 512 == 0
+        ],
+    )
 
 
-'''Return iterable of sample generator functions for the Morse for `letter`.'''
 def letter_gens(letter):
+    """Return iterable of sample generator functions for the Morse for `letter`."""
     # TODO: decide on behavior when letter not in LETTERS
     tones = LETTERS[letter]
 
     # interleave inter-symbol space silences between tones
-    for symbol in gen_join(lambda: inter_symbol, tones):
-        yield symbol
+    yield from gen_join(lambda: inter_symbol, tones)
 
-'''
-Yield a sequence of sample generator functions which represents the provided text iterable.
 
-Set suffix_space = True to append an inter-symbol space at the end of the text to allow for
-effective bandpass filtering of terminal clicks.
-'''
-def text_gens(text, suffix_space=False):
+def text_to_audio_generators(text, suffix_space=False):
+    """
+    Convert text to audio sample generators
+
+    Yield a sequence of sample generator functions which represents the provided
+    text iterable, including generators auto-interserted for inter-character spaces.
+
+    Set suffix_space = True to append an inter-symbol space at the end of the text
+    to allow for effective bandpass filtering of terminal clicks.
+    """
     chars = iter(text)
     try:
         previous_char = next(chars)
@@ -259,7 +281,8 @@ def text_gens(text, suffix_space=False):
 
     for char in chars:
         if previous_char == " ":
-            # special case for inter-word space; don't suffix it with an inter-letter space
+            # special case for inter-word space; don't suffix it with
+            # an inter-letter space
             for gen in letter_gens(previous_char):
                 yield gen
         else:
@@ -276,23 +299,27 @@ def text_gens(text, suffix_space=False):
     if suffix_space:
         yield inter_symbol
 
-'''
-Return a generator for audio samples of the Morse for `text`.
 
-n.b. this output is bandwidth limited to remove clicks, but the provided text must end with silence
-     or the filter will not be able to remove the terminal click.
-'''
-def code(text, use_bpf=True):
-    # todo: parse prosigns out of text to be encoded
-    # todo: string or list of strings passed?
+def code(text: Iterable[str], use_bpf: bool = True):
+    """
+    Return a generator for audio samples of the Morse for `text`.
 
-    # get an interable of generator functions, one per symbol/space
-    gen_funcs = text_gens(text, suffix_space=use_bpf)
+    n.b. this output is bandwidth limited to remove clicks, but the provided
+         text must end with silence or the filter will not be able to remove
+         the terminal click.
+    """
+    # TODO: parse prosigns out of text to be encoded
+    # TODO: string or list of strings passed?
+
+    # get an iterable of generator functions, one per symbol/space
+    gen_funcs = text_to_audio_generators(text, suffix_space=use_bpf)
 
     # warning consumes generator
-    #logger.debug(list(gen_funcs))
+    # logger.debug(list(gen_funcs))
 
-    logger.debug(f"Timings: DIT:{DIT} DAH:{DAH} INTER_SYMBOL:{INTER_SYMBOL} INTER_LETTER:{INTER_LETTER} INTER_WORD:{INTER_WORD}")
+    logger.debug(
+        f"Timings: DIT:{DIT} DAH:{DAH} INTER_SYMBOL:{INTER_SYMBOL} INTER_LETTER:{INTER_LETTER} INTER_WORD:{INTER_WORD}",
+    )
     logger.debug(f"HERTZ: {HERTZ}, BANDWIDTH:{BANDWIDTH}")
 
     # define bandpass filter to limit morse tone bandwidth
@@ -303,9 +330,9 @@ def code(text, use_bpf=True):
     letter_samples = itertools.chain.from_iterable(gen_func() for gen_func in gen_funcs)
 
     # Warning: Generating this debug output puts all samples in memory at once
-    #letter_samples = list(letter_samples)
-    #logger.debug(f"{len(letter_samples)} samples:")
-    #logger.debug(visualize_samples(letter_samples))
+    # letter_samples = list(letter_samples)
+    # logger.debug(f"{len(letter_samples)} samples:")
+    # logger.debug(visualize_samples(letter_samples))
 
     # chain the band pass filter three times to increase stopband attenuation
     if use_bpf:
@@ -314,9 +341,13 @@ def code(text, use_bpf=True):
         samples = letter_samples
     return samples
 
+
 if __name__ == "__main__":
-    import sys
-    #message = (sys.argv[1] if len(sys.argv) > 1 else "AC2SY").upper()
+    # message = (sys.argv[1] if len(sys.argv) > 1 else "AC2SY").upper()
     import cProfile
-    cProfile.run('audiogen.sampler.discard(code("Now is the time for all good men to come to the aid of their country ".upper() * 1, use_bpf=True))')
-    pass
+
+    cProfile.run(
+        "audiogen.sampler.discard("
+        'code("Now is the time for all good men to come to the '
+        'aid of their country ".upper() * 1, use_bpf=True))',
+    )
